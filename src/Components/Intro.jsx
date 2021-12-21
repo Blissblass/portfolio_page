90b708a33cc3e918866c60e4faf494bad8116ce0
Lily <lolpo12345@outlook.com>
2021-12-21 20:23:38 +0300

    Add logic to cycle through text

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
 import '../Stylesheets/App.css';
 
 function Intro() {
+  const intros = ["hi", "my name's Eray", "but you can call me hired ;)"];
+  const [text, setText] = useState(intros[0]);
+
+  useEffect(() => {
+    let textIdx = 0; // We already know we're starting at 0, so initialize a new index tracker here
+    let introInterval = setInterval(() => {
+      if(textIdx < intros.length - 1) {
+        console.log(textIdx);
+        textIdx += 1;
+        setText(intros[textIdx]);
+      }
+    }, 1000);
+
+    return() => {
+      clearInterval(introInterval);
+    }
+  }, []);
+
   return (
     <div className="intro">
       <header className="intro-header">
-        <p className="intro-text">hi</p>
+        <p className="intro-text">{text}</p>
       </header>
     </div>
   );
