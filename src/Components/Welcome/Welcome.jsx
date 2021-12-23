dc330cda25b8c137a23e645f64e22c7debfa429e
Lily <lolpo12345@outlook.com>
2021-12-23 21:15:01 +0300

    Create Components/Welcome and Welcome.jsx

---
+++ src/Components/Welcome/Welcome.jsx
@@ -0,0 +1,13 @@
+
+const Welcome = () => {
+
+  // First screen you see on startup, has option to go to bio or check out works
+
+  return(
+    <div>
+
+    </div>
+  )
+};
+
+export default Welcome;
