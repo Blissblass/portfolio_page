80456252689fffaffa0c47d27729e1e032c97f43
Lily <lolpo12345@outlook.com>
2021-12-29 23:43:48 +0300

    Add more text

--- src/Components/Bio/Bio.jsx
+++ src/Components/Bio/Bio.jsx
@@ -8,8 +8,13 @@ const Bio = () => {
       <div className="bio">
         <div className="bio-text">
           <p>Hello there! I'm Eray Alkış.</p>
-          <p>I started learning programming from scratch in 2021 via Ruby (and of course also Rails).</p>
-          <p>Throughout 2021, I've built many personal projects and worked with several clients to create full-stack apps.</p>
+          <p>At the start of 2021, I discovered The Odin Project and App Academy Open, open source curriculums written by experienced developers that teach almost everything needed to become a capable full-stack developer.</p>
+          <p>They both have amazing, well-written courses, and projects range from some hand-holding at the start to "Build a chess app from scratch!". I was fascinated and just <strong>had</strong> to learn how to do that.</p>
+          <p>I spent every single day of the pandemic getting stuck again and again until I figured out how to get unstuck. I made tons of mistakes and learned something from each one.</p>
+          <p>More recently though, I've been focused on learning other programming languages and mobile development.</p>
+
+          <br/>
+          <p>Currently, I'm looking to join</p>
         </div>
       </div>
     </div>
