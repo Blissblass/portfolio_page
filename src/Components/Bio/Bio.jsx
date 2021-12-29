6fffd6abae8b1779cc94531b195fdd1c8ef35f4d
Lily <lolpo12345@outlook.com>
2021-12-29 21:47:48 +0300

    Add text to bio

--- src/Components/Bio/Bio.jsx
+++ src/Components/Bio/Bio.jsx
@@ -6,9 +6,11 @@ const Bio = () => {
     <div className="bio-container">
       <h1 className="bio-header">About Me</h1>
       <div className="bio">
-        <h2>Hello!</h2>
-        <br />
-        <h4>My name's Eray Alkış. <small>(pronounced )</small></h4>
+        <div className="bio-text">
+          <p>Hello there! I'm Eray Alkış.</p>
+          <p>I started learning programming from scratch in 2021 via Ruby (and of course also Rails).</p>
+          <p>Throughout 2021, I've built many personal projects and worked with several clients to create full-stack apps.</p>
+        </div>
       </div>
     </div>
   )
