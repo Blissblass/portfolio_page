3a455025f41206fa4aaa069f874648ff995c78f5
Lily <lolpo12345@outlook.com>
2021-12-30 15:42:18 +0300

    Remove useEffect

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -2,20 +2,8 @@ import Welcome from "../Welcome/Welcome";
 import Works from "../Works/Works";
 import '../../Assets/Stylesheets/container.css'
 import Bio from "../Bio/Bio";
-import { useEffect } from "react";
 
 const Container = () => {
-
-  useEffect(() => {
-    let scrollListener = document.addEventListener("scroll", () => {
-      const bg = document.getElementById("bg");
-      bg.style.backgroundPositionX = `${window.scrollX}px`;
-    });
-
-    return() => {
-      document.removeEventListener("scroll", scrollListener);
-    }
-  })
 
   return(
     <div className="app-container">
