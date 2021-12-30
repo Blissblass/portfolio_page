24213e47cdc2f8e847f4d9f4d02eb22df3ffc083
Lily <lolpo12345@outlook.com>
2021-12-30 13:43:57 +0300

    Add bg

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -9,7 +9,7 @@ const Welcome = () => {
         <img className="welcome-image" src={placeholder} alt="My face :D" />
         <div className="welcome-text">
           <h1 className="text-1">Hi there!</h1>
-          <h1 className="text-2">I'm <span className="bio-span">Eray Alkış,</span></h1>
+          <h1 className="text-2">I'm <span className="welcome-span">Eray Alkış,</span></h1>
           <h1 className="text-3">a software developer based in Turkey.</h1>
         </div>
       </div>
