9c0759ecc1df205529e8b3a02661357e171aa833
Lily <lolpo12345@outlook.com>
2021-12-30 13:47:16 +0300

    Make text non-transparent

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -2,8 +2,19 @@ import Welcome from "../Welcome/Welcome";
 import Works from "../Works/Works";
 import '../../Assets/Stylesheets/container.css'
 import Bio from "../Bio/Bio";
+import { useEffect } from "react";
 
 const Container = () => {
+
+  useEffect(() => {
+    let scrollListener = document.addEventListener("scroll", () => {
+      const bg = document.getElementById("bg");
+    });
+
+    return() => {
+      document.removeEventListener(scrollListener);
+    }
+  })
 
   return(
     <div className="app-container">
