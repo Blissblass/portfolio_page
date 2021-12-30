5dd1fca309cea7d4f2127100f13a01bc828e0a6b
Lily <lolpo12345@outlook.com>
2021-12-30 03:37:30 +0300

    Add functionality to mail button

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,12 +4,14 @@ import { GrMail } from 'react-icons/gr';
 
 const Contact = () => {
 
+  const handleMail = () => window.location.assign("mailto:erayalkis@hotmail.com");
+
   return(
     <div className="contact-container">
       <div className="contact">
         <h1>Get In Touch</h1>
         <div className="contact-button-linkedin">Linkedin <AiFillLinkedin className="linkedin-ico" /></div>
-        <div className="contact-button-email">erayalkis@hotmail.com <GrMail className="mail-ico" /></div>
+        <div className="contact-button-email">erayalkis@hotmail.com <GrMail className="mail-ico" onClick={handleMail} /></div>
         <button className="copy-button">Copy to Clipboard</button>
       </div>
     </div>
