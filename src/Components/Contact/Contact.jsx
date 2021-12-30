633d7358818343ab9ba5769193c972a7bf8472ad
Lily <lolpo12345@outlook.com>
2021-12-30 04:22:09 +0300

    Complete functionalities for buttons

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,6 +5,10 @@ import { GrMail } from 'react-icons/gr';
 const Contact = () => {
 
   const handleMail = () => window.location.assign("mailto:erayalkis@hotmail.com");
+  const handleCopy = () => {
+    const email = document.querySelector(".contact-button-email").textContent;
+    navigator.clipboard.writeText(email);
+  };
 
   return(
     <div className="contact-container">
@@ -12,7 +16,7 @@ const Contact = () => {
         <h1>Get In Touch</h1>
         <div className="contact-button-linkedin">Linkedin <AiFillLinkedin className="linkedin-ico" /></div>
         <div className="contact-button-email">erayalkis@hotmail.com <GrMail className="mail-ico" onClick={handleMail} /></div>
-        <button className="copy-button">Copy to Clipboard</button>
+        <button className="copy-button" onClick={handleCopy}>Copy to Clipboard</button>
       </div>
     </div>
   )
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -9,7 +9,7 @@ const Works = () => {
       <div className="works-container">
         <h1 className="works-header">My works</h1> 
         <div className="works">
-          {works.map(work => <WorkItem name={work.name} link={work.link} demoLink={work.demoLink} 
+          {works.map(work => <WorkItem key={work.name} name={work.name} link={work.link} demoLink={work.demoLink} 
             description={work.description} imgLink={work.imgLink} />)}
         </div>
       </div>
