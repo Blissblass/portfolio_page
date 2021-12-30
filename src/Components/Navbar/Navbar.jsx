e3c574a5e1fd59f60aded0cc5bef2d371f3c1f9e
Lily <lolpo12345@outlook.com>
2021-12-30 20:41:03 +0300

    Add animations

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import '../../Assets/Stylesheets/navbar.css';
 
 const Navbar = () => {
+  const [showSection, setShowSection] = useState(false);
+
+  const handleClick = () => {
+    setShowSection(old => !old);
+  };
 
   return(
     <div>
       <nav className="navbar">
-        <div className="nav-button">
+        <div className="nav-button" onClick={handleClick}>
           <hr className="nav-line-1" />
           <hr className="nav-line-2"  />
           <hr className="nav-line-3"  />
         </div>
-        <div className="nav-section">
-          :3
-        </div>
+        {showSection ? 
+          <div className="nav-section">
+            :3
+          </div>
+        :
+          null  
+        }
       </nav>
     </div>
   )
