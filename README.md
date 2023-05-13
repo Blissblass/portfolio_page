8cab17de60f5ae3b565a800f3ba49680d364ed1b
Eray Alkış <lolpo12345@outlook.com>
2023-05-13 16:47:20 +0300

    Import new files

---
+++ README.md
@@ -0,0 +1,71 @@
+<p align="center">
+  <img alt="logo" src="./public/images/MB-mono.svg" width="60" />
+</p>
+<h1 align="center">
+  my-site
+</h1>
+
+![my-site status](https://img.shields.io/website?down_color=red&down_message=offline&label=my-site&up_message=online&url=https%3A%2F%2Fmanpreetbhatti.com%2F)
+
+![my-site release](https://img.shields.io/github/release/Manpreet-Bhatti/my-site-2.svg?style=popout-square) ![my-site issues](https://img.shields.io/github/issues/Manpreet-Bhatti/my-site-2.svg?style=popout-square) ![my-site top language](https://img.shields.io/github/languages/top/Manpreet-Bhatti/my-site-2.svg?style=popout-square) ![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB) ![Next.js](https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=scss&logoColor=white) ![styled-components](https://img.shields.io/badge/styled&#x2010;components-DD82CA?style=for-the-badge&logo=styled-components&logoColor=white) ![my-site last commit](https://img.shields.io/github/last-commit/Manpreet-Bhatti/my-site-2.svg?style=popout-square) ![my-site license](https://img.shields.io/github/license/Manpreet-Bhatti/my-site-2) [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
+### Personal website of Manpreet Bhatti
+
+Check out this [live demo](https://manpreetbhatti.com/) to see my-site in action.
+
+# Getting started
+
+## Let's Install
+
+Install modules
+
+```shell
+cd my-site
+npm install
+```
+
+## Let's Build
+
+Start a development server
+
+```shell
+npm run dev
+```
+
+or
+Create and start a production build
+
+```shell
+npm run build
+npm run start
+```
+
+## Let's Customize
+
+> Fork this project for your customization ideas!
+>
+> Tip: You should see if these changes work in the development server before building and starting.
+
+### Fonts
+
+To add custom fonts, make sure to import them into `./components/fonts.js` and change `./components/logo.js` and `./lib/theme.js` accordingly.
+
+### Colours
+
+To change colours (or add your own), edit `./lib/theme.js` to your liking. There are many instances where colours weren't used from the theme so keep an eye out (colours will be more centralized in the near future)!
+
+### Skills
+
+To showcase your skills, edit `./pages/projects.js` and add your projects to `./pages/projects` and/or edit `./pages/work.js` and add your work to `./pages/work`!
+
+### Socials
+
+Let others know where they can find you by editing `./pages/index.js` and add your own contact links near the end.
+
+## Let's Publish
+
+There are many great hosting platforms that will have services to showcase your static websites. I used Next.js for this website which goes perfecty with [Vercel](https://vercel.com/); I advise you use it!
+
+## License
+
+![my-site license](https://img.shields.io/github/license/Manpreet-Bhatti/my-site-2)
---
+++ pages/index.js
@@ -0,0 +1,209 @@
+import NextLink from "next/link";
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra,
+  Text,
+} from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import Paragraph from "../components/paragraph";
+import Layout from "../components/layouts/single-column";
+import Section from "../components/section";
+import { GridItem } from "../components/grid-item";
+import { IoLogoLinkedin, IoLogoGithub, IoMail } from "react-icons/io5";
+import Image from "next/image";
+import theme from "../lib/theme";
+
+import thumbGrafGas from "../public/images/projects/grafgas.jpg";
+import thumbAutoReadmeDocs from "../public/images/projects/autoreadmedocs.jpg";
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: (prop) => ["width", "height", "src", "alt"].includes(prop),
+});
+
+const Home = () => (
+  <Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue("#f4f4f4", "whiteAlpha.200")}
+        css={{ backdropFilter: "blur(10px)" }}
+      >
+        I&apos;m a front end (aspiring full stack) developer based in{" "}
+        <span title="Canada" aria-label="Canada">
+          🇨🇦
+        </span>
+        !
+      </Box>
+      <Box display={{ md: "flex" }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Manpreet Bhatti
+          </Heading>
+          <Text color={theme.colors.redLight}>
+            Front-end Engineer @ Agentnoon
+          </Text>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor={theme.colors.redLight}
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/manpreet-coding.png"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
+        </Box>
+      </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          👨🏽‍💻 Who I am
+        </Heading>
+        <Paragraph>
+          I was formerly a front end developer at{" "}
+          <Link variant="red-link" href="https://criticalmass.com/" isExternal>
+            Critical Mass
+          </Link>
+          , where I built various features and components for Nissan and
+          Infiniti. Based in Ontario, I have a passion for all things front end
+          with a keen mind to create beautiful, intuitive, and performant user
+          experiences. However, I look to grow as a developer and transition
+          into a full stack environment. I am currently in search of full-time
+          opportunities in hopes of starting a new challenge.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/work" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} variant="red-button">
+              My experience
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          🤝 Community involvement
+        </Heading>
+        <Paragraph>
+          Giving back to my community is a joy for me; I help develop for my
+          university&apos;s hackathon,{" "}
+          <Link variant="red-link" href="https://hackthevalley.io/" isExternal>
+            Hack The Valley
+          </Link>
+          , as well as teach and mentor aspiring developers.
+        </Paragraph>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          🍿 Off-hours
+        </Heading>
+        <Paragraph>
+          When I&apos;m not developing, one of my hobbies is watching movies and
+          TV shows. For exploration and critical analysis purposes, I like to
+          immerse myself in all types of genres. That being said, action,
+          mystery, and sci-fi are the go-to.
+        </Paragraph>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          🕸 On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link
+              href="https://www.linkedin.com/in/manpreet1bhatti/"
+              isExternal
+            >
+              <Button
+                variant="ghost"
+                colorScheme="red"
+                color={theme.colors.redLight}
+                leftIcon={<IoLogoLinkedin />}
+              >
+                manpreet1bhatti
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://github.com/Manpreet-Bhatti" isExternal>
+              <Button
+                variant="ghost"
+                colorScheme="red"
+                color={theme.colors.redLight}
+                leftIcon={<IoLogoGithub />}
+              >
+                Manpreet-Bhatti
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="mailto:manpreet@bhatti.net" isExternal>
+              <Button
+                variant="ghost"
+                colorScheme="red"
+                color={theme.colors.redLight}
+                leftIcon={<IoMail />}
+              >
+                manpreet [at] bhatti.net
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Section>
+      <Section delay={0.4}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="https://www.grafgas.com/"
+            title="Graf-Gas"
+            thumbnail={thumbGrafGas}
+          >
+            A data analytical dashboard with a focus on fleet driver fuel
+            consumption and transportation efficiency
+          </GridItem>
+          <GridItem
+            href="https://project-structure-readme.netlify.app/"
+            title="Auto README Docs"
+            thumbnail={thumbAutoReadmeDocs}
+          >
+            A web application that helps automate the README creation of GitHub
+            repository file structures
+          </GridItem>
+        </SimpleGrid>
+        <Box align="center" my={4}>
+          <NextLink href="/posts" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} variant="red-button">
+              My projects
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+    </Container>
+  </Layout>
+);
+
+export default Home;
+export { getServerSideProps } from "../components/chakra";
